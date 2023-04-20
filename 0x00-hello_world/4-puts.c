dd8185ad1014232b1bb0eaa3c623bb17996570b5
lyneateka <linateka98@gmail.com>
2023-04-20 10:29:18 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - entry point
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (o);
+}
