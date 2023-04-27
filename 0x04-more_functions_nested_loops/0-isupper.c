6a48c075aca3dbbc11c8e17ce35e0114838ce4bd
lyneateka <linateka98@gmail.com>
2023-04-27 07:26:43 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,12 +4,16 @@
  * _isupper - uppercase letters
  * @c: char to check
  *
- * Return 0 or 1
+ * Return: 1 if character is uppercase, 0 otherwise.
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
