ade121b19530d1b9b1bd73172db0f6e9c790b7c2
lyneateka <linateka98@gmail.com>
2023-04-28 01:07:00 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int n;
+	int i;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar ((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
